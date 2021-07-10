e8d52dc758882e417be6979c05f539a100c662a2
yukaasato <newadgjyj@gmail.com>
2021-07-10 19:30:46 +0900

    init

---
+++ src/main/java/com/tasks/TasksManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksManagerApplication.class, args);
+	}
+
+}
